eb7007208fadd83c75c6f1e26c0937611c56eb1e
Aleks Bujko <aleksander.bujko@unterschiedundmacher.com>
2021-02-23 18:36:30 +0100

    fextracted header into an extra component

--- social-media-dashboard/src/app/app.module.ts
+++ social-media-dashboard/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SocialMediaKpiComponent } from './social-media-kpi/social-media-kpi.component';
 import { SocialMediaChannelComponent } from './social-media-channel/social-media-channel.component';
+import { HeadingAreaComponent } from './heading-area/heading-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SocialMediaKpiComponent,
-    SocialMediaChannelComponent
+    SocialMediaChannelComponent,
+    HeadingAreaComponent
   ],
   imports: [
     BrowserModule
